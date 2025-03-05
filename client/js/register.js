b38fa2546f0f0b0919c16c6265d11694599947f8
bdaly19 <bdaly0809@gmail.com>
2025-03-05 14:21:05 -0500

    created register.js

---
+++ client/js/register.js
@@ -0,0 +1,23 @@
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    console.log('registered');
+
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, passwords: password, email })
+    });
+    console.log('response received');
+    if (response.ok) {
+        alert('Registration successful! You can now log in.');
+        window.location.href = '/client/index.html';
+    } else {
+        alert('Registration failed. Please try again.');
+    }
+});
